35efbe08313d351c4edb2134fadb92c2437bdf88
fabioaverbuch <fabio.averbuch@hotmail.com>
2022-07-21 10:07:41 -0300

    20.07.22

--- DBA.Oracle.Scripts/ora_xplan.display.sql
+++ DBA.Oracle.Scripts/ora_xplan.display.sql
@@ -3,24 +3,27 @@ EXPLAIN PLAN SET STATEMENT_ID = '<STATEMENT_NAME>' FOR SELECT ....
 
 SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY('PLAN_TABLE','<STATEMENT_NAME>','BASIC | TYPICAL | SERIAL | ALL'));
 									   
-BASIC ..... displays minimum information
-TYPICAL ... displays most relevant information
-SERIAL .... like TYPICAL but without parallel information
-ALL ....... displays all information
 									   
-DBMS_XPLAN.DISPLAY.DISPLAY_CURSOR
-Display from GV$SQL_PLAN (or GV$SQL_PLAN_STATISTICS_ALL)
-									   
-DBMS_XPLAN.DISPLAY_PLAN
-Return the last plan, or a named plan, explained as a CLOB
+BASIC: Exibe o minimo de informações sobre o plano de execução: O ID da operação, o nome da operação e o nome do objeto que será tratado pela operação.
 
-DBMS_XPLAN.DISPLAY_SQLSET
-Format and display the contents of the execution plan of statements stored in a SQL tuning set
-									   
-DBMS_XPLAN.DISPLAY_SQL_PLAN_BASELINE
-Displays one or more execution plans for the specified sql_handle of a SQL statement
-									   
-DBMS_XPLAN.DISPLAY_WORKLOAD_REPOSITORY
-Display from AWR								   
+TYPICAL: Exibe as informações mais relevantes do plano de execução, se o parâmetro “Format” for omitido (null) esse será o valor considerado. 
 
+SERIAL: Exibe quase as mesmas informações que o formato “Typical” com exceção das informações das operações de paralelismo que serão omitidas mesmo que o plano utilize o paralelismo na sua execução.
+									   
+ADVANCED: Exibe as mesmas informações que o formato “Typical” e inclui algumas sessões de informações não estatísticas sobre o plano de execução.
+									   
+									   
+									   
+MEMSTATS: Exibe estatística referente a estimativa da quantidade de espaço a ser utilizado na PGA para operações de SORT, HASH JOIN, BITMAP MERGE ou BITMAP CREATE. 
 
+IOSTATS: Exibe estatísticas referentes as operações de “IO” executadas para um determinado plano de execução. 
+
+ALLSTATS: Exibe todas as informações estatísticas fornecidas por MEMSTATS e IOSTATS.
+									   
+
+									   
+LAST: Se for informada a palavra chave LAST serão exibidos valores estatísticos somente da ultima execução. 
+
+ALL: Essa palavra chave permite que sejam exibidas sessões adicionais (Query Block Name e Column Projection) de informações não estatísticas que aparecem logo após o plano de execução. 
+
+ 
